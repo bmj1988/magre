8e75c31bc10dacdbcce3c7ca327262bc9547cee8
Brian Johnson <126127671+bmj1988@users.noreply.github.com>
2024-09-14 18:25:51 -0400

    fixed redux, just need it to check against master before the fix

--- ichingMobile/App.jsx
+++ ichingMobile/App.jsx
@@ -1,11 +1,15 @@
 import { ReadingProvider } from './src/context/readingContext';
 import RootNavigation from './src/navigation';
 import { Provider } from 'react-redux'
+import { store } from './store';
 
 export default function App() {
+
   return (
+    <Provider store={store}>
       <ReadingProvider>
         <RootNavigation />
       </ReadingProvider>
+    </Provider>
   )
 }
